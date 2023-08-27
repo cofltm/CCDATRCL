1244cd5554020eb399e4cbeeef9232d6a15d19f9
Cherish Sarmiento <111546406+cofltm@users.noreply.github.com>
2023-08-27 23:10:23 +0800

    Create Act1_1.java

---
+++ Act1/Act1_1.java
@@ -0,0 +1,125 @@
+/* Cherish Sarmiento
+   INF227
+   
+   Edit the program; add statement to find Odd numbers as well
+
+ */
+
+public class Act1_1 { //EvenNumber
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 100; i++){
+            if (i % 2 == 0){
+                System.out.println(i + " is an EVEN NUMBER");
+            }
+            else{
+                System.out.println(i + " is an ODD NUMBER");
+            }
+        }
+    }
+}
+
+/* output:
+
+1 is an ODD NUMBER
+2 is an EVEN NUMBER
+3 is an ODD NUMBER 
+4 is an EVEN NUMBER
+5 is an ODD NUMBER 
+6 is an EVEN NUMBER
+7 is an ODD NUMBER
+8 is an EVEN NUMBER
+9 is an ODD NUMBER
+10 is an EVEN NUMBER
+11 is an ODD NUMBER
+12 is an EVEN NUMBER
+13 is an ODD NUMBER
+14 is an EVEN NUMBER
+15 is an ODD NUMBER
+16 is an EVEN NUMBER
+17 is an ODD NUMBER
+18 is an EVEN NUMBER
+19 is an ODD NUMBER
+20 is an EVEN NUMBER
+21 is an ODD NUMBER
+22 is an EVEN NUMBER
+23 is an ODD NUMBER
+24 is an EVEN NUMBER
+25 is an ODD NUMBER
+26 is an EVEN NUMBER
+27 is an ODD NUMBER
+28 is an EVEN NUMBER
+29 is an ODD NUMBER
+30 is an EVEN NUMBER
+31 is an ODD NUMBER
+32 is an EVEN NUMBER
+33 is an ODD NUMBER
+34 is an EVEN NUMBER
+35 is an ODD NUMBER
+36 is an EVEN NUMBER
+37 is an ODD NUMBER
+38 is an EVEN NUMBER
+39 is an ODD NUMBER
+40 is an EVEN NUMBER
+41 is an ODD NUMBER
+42 is an EVEN NUMBER
+43 is an ODD NUMBER
+44 is an EVEN NUMBER
+45 is an ODD NUMBER
+46 is an EVEN NUMBER
+47 is an ODD NUMBER
+48 is an EVEN NUMBER
+49 is an ODD NUMBER
+50 is an EVEN NUMBER
+51 is an ODD NUMBER
+52 is an EVEN NUMBER
+53 is an ODD NUMBER
+54 is an EVEN NUMBER
+55 is an ODD NUMBER
+56 is an EVEN NUMBER
+57 is an ODD NUMBER
+58 is an EVEN NUMBER
+59 is an ODD NUMBER
+60 is an EVEN NUMBER
+61 is an ODD NUMBER
+62 is an EVEN NUMBER
+63 is an ODD NUMBER
+64 is an EVEN NUMBER
+65 is an ODD NUMBER
+66 is an EVEN NUMBER
+67 is an ODD NUMBER
+68 is an EVEN NUMBER
+69 is an ODD NUMBER
+70 is an EVEN NUMBER
+71 is an ODD NUMBER
+72 is an EVEN NUMBER
+73 is an ODD NUMBER
+74 is an EVEN NUMBER
+75 is an ODD NUMBER
+76 is an EVEN NUMBER
+77 is an ODD NUMBER
+78 is an EVEN NUMBER
+79 is an ODD NUMBER
+80 is an EVEN NUMBER
+81 is an ODD NUMBER
+82 is an EVEN NUMBER
+83 is an ODD NUMBER
+84 is an EVEN NUMBER
+85 is an ODD NUMBER
+86 is an EVEN NUMBER
+87 is an ODD NUMBER
+88 is an EVEN NUMBER
+89 is an ODD NUMBER
+90 is an EVEN NUMBER
+91 is an ODD NUMBER
+92 is an EVEN NUMBER
+93 is an ODD NUMBER
+94 is an EVEN NUMBER
+95 is an ODD NUMBER
+96 is an EVEN NUMBER
+97 is an ODD NUMBER
+98 is an EVEN NUMBER
+99 is an ODD NUMBER
+100 is an EVEN NUMBER
+
+*/
